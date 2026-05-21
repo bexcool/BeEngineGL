aa6a2e46ae4c7bffa2d6ef349557e5dae0da863b
Peter <53868994+Pietro228@users.noreply.github.com>
2026-05-21 10:06:21 +0200

    Made this project a library

---
+++ build/CMakeFiles/BeEngineGL.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/logger.c" "CMakeFiles/BeEngineGL.dir/Source/Core/logger.c.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/logger.c.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/App.cpp" "CMakeFiles/BeEngineGL.dir/Source/App.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/App.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Application.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Application.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Application.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Controllers/Controller.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Controllers/Controller.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Controllers/Controller.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Controllers/PlayerController.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Controllers/PlayerController.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Controllers/PlayerController.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Events/EventArgs.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/EventArgs.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/EventArgs.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Events/EventDispatcher.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/EventDispatcher.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/EventDispatcher.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Events/InputManager.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/InputManager.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/InputManager.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Events/KeyboardKeyEventArgs.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/KeyboardKeyEventArgs.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/KeyboardKeyEventArgs.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Events/MouseKeyEventArgs.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/MouseKeyEventArgs.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Events/MouseKeyEventArgs.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/GameLoop.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/GameLoop.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/GameLoop.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Material/Material.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/Material.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/Material.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Material/MaterialParameter.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/MaterialParameter.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/MaterialParameter.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Material/Texture.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/Texture.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Material/Texture.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Model.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Model.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Model.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Renderer.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Renderer.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Renderer.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Shader.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Shader.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Shader.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/ShaderInfo.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/ShaderInfo.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/ShaderInfo.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/ShaderProgram.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/ShaderProgram.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/ShaderProgram.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/SkyBox/SkyBoxModel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/SkyBox/SkyBoxModel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/SkyBox/SkyBoxModel.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/SkyBox/SkyBoxShaderProgram.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/SkyBox/SkyBoxShaderProgram.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/SkyBox/SkyBoxShaderProgram.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Graphics/Window.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Window.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Graphics/Window.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Interfaces/ICoreEvents.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/ICoreEvents.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/ICoreEvents.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Interfaces/IDisposable.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/IDisposable.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/IDisposable.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Interfaces/IKeyEvents.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/IKeyEvents.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Interfaces/IKeyEvents.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Level.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Level.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Level.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Math/Location.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Location.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Location.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Math/Rotation.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Rotation.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Rotation.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Math/Scale.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Scale.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Scale.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Math/Transform.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Transform.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Transform.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Math/Vector3.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Vector3.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Math/Vector3.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/ObjectComponents/CameraComponent.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/CameraComponent.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/CameraComponent.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/ObjectComponents/GameObjectComponent.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/GameObjectComponent.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/GameObjectComponent.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/ObjectComponents/LightComponent.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/LightComponent.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/LightComponent.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/ObjectComponents/ModelComponent.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/ModelComponent.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/ModelComponent.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/ObjectComponents/SplineComponent.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/SplineComponent.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/ObjectComponents/SplineComponent.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Objects/Character/Character.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/Character/Character.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/Character/Character.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Objects/Character/PlayerCharacter.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/Character/PlayerCharacter.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/Character/PlayerCharacter.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Core/Objects/GameObject.cpp" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/GameObject.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Core/Objects/GameObject.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Game/BallsLevel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Game/BallsLevel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Game/BallsLevel.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Game/MonkeyLevel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Game/MonkeyLevel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Game/MonkeyLevel.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Game/SolarSystemLevel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Game/SolarSystemLevel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Game/SolarSystemLevel.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Game/TestLevel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Game/TestLevel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Game/TestLevel.cpp.o.d"
+  "/Volumes/Santuv M.2 Drive/Programming/C++/BeEngineGL/Source/Game/WhackMoleLevel.cpp" "CMakeFiles/BeEngineGL.dir/Source/Game/WhackMoleLevel.cpp.o" "gcc" "CMakeFiles/BeEngineGL.dir/Source/Game/WhackMoleLevel.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BeEngineGL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BeEngineGL.
+# This may be replaced when dependencies are built.
