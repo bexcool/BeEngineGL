bb0e48bcf7e4532ca74be1a3c562eaac26d32064
Peter <53868994+Pietro228@users.noreply.github.com>
2026-05-21 10:46:47 +0200

    Deleted build

--- build/CMakeFiles/copyResources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copyResources"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copyResources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
