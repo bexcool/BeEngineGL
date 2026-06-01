7040fc3a6e46707723881e7da9cdcef1297e7bfd
Peter <53868994+Pietro228@users.noreply.github.com>
2026-06-01 10:20:25 +0200

    Increased far clipping plane

--- Source/BECore/ObjectComponents/CameraComponent.cpp
+++ Source/BECore/ObjectComponents/CameraComponent.cpp
@@ -47,7 +47,7 @@ glm::mat4 CameraComponent::GetCameraProjectionMatrix()
 {
     float aspectRatio = Application::GetInstance()->GetWindow()->GetAspectRatio();
 
-    return glm::perspective(glm::radians(GetFOV()), aspectRatio, 0.1f, 100.0f);
+    return glm::perspective(glm::radians(GetFOV()), aspectRatio, 0.1f, 1000.0f);
 }
 
 void CameraComponent::SetSensitivity(float sensitivity)
